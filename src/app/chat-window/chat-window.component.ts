e61c6b6c6d6e6a7d8e39ea3aa0d06adf5b1c0970
Jakim Hernández <egalink@gmail.com>
2020-02-23 00:26:24 -0600

    [ADD] - Appending new messages written by the user on chat window (Functionally on front-end side only).

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,13 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-chat-window',
     templateUrl: './chat-window.component.html',
     styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, AfterViewChecked  {
+
+    @ViewChild('messageList') messageListRef:ElementRef;
 
     opened:boolean = false;
+
+    messages:any[] = [
+        {
+            message: 'Hola galletita, ¿como estas?',
+            sender: { username: 'Egalink Hdz.', id: 'me' },
+            sentat: '22-02-2020T18:00:00',
+        },
+        {
+            message: 'Hola Rana Ranosa!',
+            sender: { username: 'Mafer Mad.', id: 'in' },
+            sentat: '22-02-2020T18:02:01',
+        },
+        {
+            message: 'Muy bien, aqui.',
+            sender: { username: 'Mafer Mad.', id: 'in' },
+            sentat: '22-02-2020T18:02:05',
+        },
+        {
+            message: 'Haciendo la interfáz gráfica del chat.',
+            sender: { username: 'Mafer Mad.', id: 'in' },
+            sentat: '22-02-2020T18:02:12',
+        },
+        {
+            message: 'y tu?',
+            sender: { username: 'Mafer Mad.', id: 'in' },
+            sentat: '22-02-2020T18:02:18',
+        },
+        {
+            message: 'Aqui, extrañando a la galleta.',
+            sender: { username: 'Egalink Hdz.', id: 'me' },
+            sentat: '22-02-2020T18:03:15',
+        },
+        {
+            message: 'Hay algo en lo que te pueda ayudar? ...',
+            sender: { username: 'Egalink Hdz.', id: 'me' },
+            sentat: '22-02-2020T18:03:22',
+        }
+    ];
 
     constructor () {
         //
@@ -17,9 +58,44 @@ export class ChatWindowComponent implements OnInit {
         //
     }
 
+    ngAfterViewChecked () {
+        //
+        this.scrollToLastMessages();
+    }
+
+    scrollToLastMessages () {
+        // scroll down to bottom when new message is sent:
+        const messageList:any = this.messageListRef.nativeElement;
+        messageList.scrollTop = messageList.scrollHeight;
+    }
+
     openSidenav ($sidenav) : void {
         //
         $sidenav.toggle();
+    }
+
+    sendMessage ($input) {
+        //
+        console.log($input.value);
+
+        if (! $input.value) {
+            return;  // empty field.
+        }
+
+        let message:any = {
+            message: $input.value || '',
+            sender: { username: 'Egalink Hdz.', id: 'me' },
+            sentat: moment().format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS	)
+        };
+ 
+        this.messages.push(message);
+        this.scrollToLastMessages();
+        $input.value = '';
+    }
+
+    obj (obj:any = {}) {
+        //
+        return obj;
     }
 
 }
